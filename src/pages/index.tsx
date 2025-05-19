a90b9bc6ded54a64fe6ff96661cd1adc0ff85e26
JuanVasquez <juan.vasquez21@udea.edu.co>
2025-05-19 16:48:50 -0500

    Implement code changes to enhance functionality and improve performance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
-  <div className="block mx-auto mb-4 w-16">
-        <Image
-          src="/assets/logo.png" // Ruta a la imagen en la carpeta "public"
-          alt="CourierSync Logo"
-          width={64} // Especifica el ancho de la imagen
-          height={64} // Especifica la altura de la imagen
-        />
-      </div>
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/usuarios');
+  }, [router]);
+
+  return null; // opcionalmente muestra un loader aquí
 }
