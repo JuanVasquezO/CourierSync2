e0afbbb040627997c0464d8412f66a677e1c3fe4
JuanVasquez <juan.vasquez21@udea.edu.co>
2025-05-19 17:29:53 -0500

    s

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/atoms/Button.tsx
-export default function Home({ text }: { text: string }) {
-  return (
-    <button className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-      {text}
-    </button>
-  );
-}
